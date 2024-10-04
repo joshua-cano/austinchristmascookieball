504cfd4620e27932922763146860f756fea7fe63
Joshua Cano <joshuacano@unknown028365261fe2.attlocal.net>
2024-10-03 20:19:27 -0500

    Autoplay video

--- src/components/custom-video.tsx
+++ src/components/custom-video.tsx
@@ -1,48 +1,41 @@
 "use client";
 
-import { useState } from "react";
 import thumbnail from "../../public/thumbnail.png";
 import Image from "next/image";
-import ReactPlayer from "react-player/youtube";
 import { FaPlay } from "react-icons/fa";
+import dynamic from "next/dynamic";
+import { useMediaQuery } from "usehooks-ts";
+
+const ReactPlayer = dynamic(() => import("react-player/youtube"), {
+  ssr: false,
+});
+
+const PlayButton = () => {
+  return (
+    <div className="absolute grid place-items-center text-[#CC2727] text-4xl size-20 rounded-full bg-white">
+      <FaPlay aria-label="Play Video" size={36} />
+    </div>
+  );
+};
 
 export const CustomVideo = () => {
-  const [isPlaying, setIsPlaying] = useState(false);
-
-  // Function to handle play button click
-  const handlePlay = () => {
-    setIsPlaying(true);
-  };
+  const matches = useMediaQuery("(min-width: 768px");
 
   return (
     <div className="relative">
-      {isPlaying ? (
-        // Show YouTube iframe when play button is clicked
-        <div className="aspect-[16/11] md:rounded-2xl overflow-hidden">
-          <ReactPlayer
-            url={`https://www.youtube.com/watch?v=b6XPvDiYWM8?si=jg41Q3dVk5u-_Kb_`}
-            playing
-            controls
-            width="100%"
-            height="100%"
-          />
-        </div>
-      ) : (
-        // Show custom thumbnail with play button when isPlaying is false
-        <div className="relative cursor-pointer" onClick={handlePlay}>
-          <Image
-            src={thumbnail}
-            alt="Video thumbnail"
-            className="md:rounded-2xl"
-          />
-          {/* Custom Play button */}
-          <div className="absolute inset-0 grid place-items-center">
-            <button className="grid place-items-center text-[#CC2727] text-4xl size-20 rounded-full bg-white">
-              <FaPlay size={36} />
-            </button>
-          </div>
-        </div>
-      )}
+      <div className="aspect-[16/11] md:rounded-2xl overflow-hidden">
+        <ReactPlayer
+          url="https://www.youtube.com/watch?v=b6XPvDiYWM8"
+          playing
+          muted={!matches}
+          controls
+          width="100%"
+          height="100%"
+          playIcon={<PlayButton />}
+          playsinline
+          light={<Image src={thumbnail} alt="Thumbnail" />}
+        />
+      </div>
     </div>
   );
 };
